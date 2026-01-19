358e28e89c06698f7f03a05cdbbc03acf2d3a048
javi <jefalcon@gmail.com>
2026-01-19 16:57:03 +0100

    capitulo 3 finalizado

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Get;
+use App\Models\State; // Ajusta esto si tu modelo está en otra carpeta
+use App\Models\City; // Ajusta esto si tu modelo está en otra carpeta
+use Illuminate\Support\Collection;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'tabler-users';
+
+    //Con esto renombras el nombre del CRUD en la pantalla dashboard
+    protected static ?string $navigationLabel = 'Employees';
+    protected static ?string $navigationGroup = 'Employees Management';
+    protected static ?int $navigationSort = 2;
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                
+                Section::make('Personal Information')
+                ->columns(3)
+                ->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->label('Nombre Completo')
+                        ->required(),
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->required(),
+                    Forms\Components\TextInput::make('password')
+                        ->password()
+                        ->required(),
+                        ]),
+                Section::make('Adress Information')
+                ->columns(3)
+                ->schema([
+                    Forms\Components\Select::make('country_id')
+                        ->relationship(name: 'country', titleAttribute: 'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(function (Get $get, Forms\Set $set) {$set('state_id', null); $set('city_id', null);} )
+                        ->required(),
+                    Forms\Components\Select::make('state_id')
+                        ->options(fn (Get $get): Collection=>State::query()
+                            ->where('country_id', $get('country_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(function (Get $get, Forms\Set $set) {$set('city_id', null);} )
+                        ->required(),
+                    Forms\Components\Select::make('city_id')
+                        ->options(fn (Get $get): Collection=>City::query()
+                            ->where('state_id', $get('state_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+                    Forms\Components\TextInput::make('address')
+                        ->required(),
+                    Forms\Components\TextInput::make('postal_code')
+                        ->required()
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nombre Completo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('postal_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Timesheet.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Timesheet extends Model
+{
+    //
+    Use HasFactory;
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'table_user_departament', 'departament_id', 'user_id');
+    }
+    public function calendar()
+    {
+        return $this->belongsTo(Calendar::class);
+    }
+
+}
